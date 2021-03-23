e92d8ee4a3f46a9163427e07d3363c630572b8c9
xs1hao <42058817+xs1hao@users.noreply.github.com>
2021-03-23 22:50:18 +0800

    2021年3月23日22:50:16

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+          hello sq react 这是我的react 测试项目~~
+      </p>
+    </div>
+  );
+}
+
+export default App;
